a30b6d7cc3006130c796078c28d05ca8edf1fe6e
euglevi <58224469+euglevi@users.noreply.github.com>
2020-12-06 14:33:47 +0100

    first commit

--- plugin/copy_stata.zsh
+++ plugin/copy_stata.zsh
@@ -5,6 +5,7 @@ stata_window="$(xdotool search --name --limit 1 "Stata/(IC|SE|MP)")"
 this_window="$(xdotool getactivewindow)" &&
 xdotool \
         keyup ctrl shift \
+	sleep 2 \
         windowactivate --sync $stata_window \
         key --clearmodifiers --delay 100 ctrl+v Return \
         windowactivate --sync $this_window
