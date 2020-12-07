90b03c4a93ac3496d3eafb5e4dffd48f5665313e
euglevi <58224469+euglevi@users.noreply.github.com>
2020-12-07 14:02:23 +0100

    first commit

--- plugin/copy_stata.zsh
+++ plugin/copy_stata.zsh
@@ -6,6 +6,6 @@ this_window="$(xdotool getactivewindow)"
 xdotool \
         keyup ctrl shift \
         windowactivate --sync $stata_window \
-        key --clearmodifiers --delay 10 ctrl+v Return \
+        key --clearmodifiers --delay 100 ctrl+v Return \
         windowactivate --sync $this_window 
         #key --clearmodifiers --delay 100 Escape g g V G Escape 
